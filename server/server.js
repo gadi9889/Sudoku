bcb60613579c54acc2937b3fbbf9eb2e648374ec
gadi <gadi8998@gmail.com>
2022-02-05 21:50:56 +0200

    env set up for heruko

--- server/server.js
+++ server/server.js
@@ -7,7 +7,7 @@ const path = require('path')
 
 const mongoose = require('mongoose')
 
-mongoose.connect(process.env.DATABASE_URL, {useNewUrlParser:true});
+mongoose.connect(process.env.MONGODB_URI, {useNewUrlParser:true});
 
 const db = mongoose.connection;
 db.on('error', (err) => console.error(err));
